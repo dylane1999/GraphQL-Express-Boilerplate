9ec40d1a59bf933374a2ef314f274c6b3a481062
Dylan Edwards <dylanedwards290@gmail.com>
2020-12-20 19:51:50 -0800

    add graph ql endpoint

--- server.js
+++ server.js
@@ -1,20 +1,38 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-// const bodyParser = require('body-parser');
-
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+var cors = require("cors");
+var dotenv = require("dotenv");
 dotenv.config({ path: "./.env" });
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-// app.use(bodyParser);
 app.use(express.urlencoded({ extended: false }));
 
-require("./routes/route1")(app);
-require("./routes/route2.js")(app);
+ 
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+ 
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+ 
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
-var port = process.env.PORT;
+
+var port = process.env.PORT || 5000;
 
 app.listen(port, () => {
 	console.log('server started on port 5000');
